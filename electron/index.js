e0faa705929d1098deb891fc609efaadb8304f29
astrit <am@astrit.co>
2024-04-14 20:29:07 +0200

    Init

--- electron/index.js
+++
@@ -1,8 +0,0 @@
-const { app, BrowserWindow } = require("electron");
-
-let mainWindow;
-
-app.on("ready", () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadFile("index.html");
-});
